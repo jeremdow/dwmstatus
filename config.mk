bb23ec40f227b63dc21a0efad20577592d0a1a5d
Jeremiah Dow <jeremdow@gmail.com>
2013-12-18 11:56:10 -0500

    remove x11 libs from makefile config

--- config.mk
+++ config.mk
@@ -7,18 +7,18 @@ VERSION = 1.2
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
-
 # includes and libs
-INCS = -I. -I/usr/include -I${X11INC}
-LIBS = -L/usr/lib -lc -L${X11LIB} -lX11
+INCS = -I. -I/usr/include
+LIBS = -L/usr/lib -lc
 
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\"
-CFLAGS = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-#CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
-LDFLAGS = -g ${LIBS}
+#CPPFLAGS = -DVERSION=\"${VERSION}\" -D_FORTIFY_SOURCE=2
+#CFLAGS = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
+CFLAGS = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS} -march=native -O2 -pipe -fstack-protector --param=ssp-buffer-size=4
+CXXFLAGS = ${CFLAGS}
+#LDFLAGS = -g ${LIBS}
+LDFLAGS = -g ${LIBS} -Wl,-O1,--sort-common,--as-needed,-z,relro
 #LDFLAGS = -s ${LIBS}
 
 # Solaris
